d21e58e8d67b222b188905671ea1c75b76031591
lslemon <l.slemon1@nuigalway.ie>
2020-02-11 20:42:18 +0000

    Edited Login

--- src/client/Log_in.java
+++ src/client/Log_in.java
@@ -1,14 +1,20 @@
 package client;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Log_in
 {
     private JPanel rootPanel;
-    private JPasswordField passwordField1;
-    private JTextField textField1;
+    private JPanel loginPanel;
+    private JLabel welcomeLabel = new JLabel("Welcome to NUIG Assessments");
+    private JLabel loginLabel = new JLabel("Please Log in");
+    private JLabel studentIDLabel = new JLabel("Student ID");
+    private JLabel passwordLabel = new JLabel("Password");
+    private JPasswordField passwordField;
+    private JTextField studentIdField;
     private PanelListener listener;
 
     public interface PanelListener
@@ -19,6 +25,23 @@ public class Log_in
 
     public Log_in()
     {
+        rootPanel = new JPanel();
+        rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.PAGE_AXIS));
+        rootPanel.add(welcomeLabel);
+        rootPanel.add(loginLabel);
+
+        loginPanel = new JPanel();
+        loginPanel.setLayout(new BoxLayout(loginPanel, BoxLayout.PAGE_AXIS));
+
+        passwordField = new JPasswordField();
+        studentIdField = new JTextField();
+
+        loginPanel.add(studentIDLabel);
+        loginPanel.add(studentIdField);
+        loginPanel.add(passwordLabel);
+        loginPanel.add(passwordField);
+
+        rootPanel.add(loginPanel);
 
     }
 
